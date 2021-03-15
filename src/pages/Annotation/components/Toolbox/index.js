01357bf0c3382cbcf3485340616edbd8dc9e9862
Thuc Nguyen <zthucnguyen99@gmail.com>
2021-03-15 15:24:25 +0700

    feat: toolbox
    - select icon
    - rectangle mode
    - polygon mode

--- src/pages/Annotation/components/Toolbox/index.js
+++ src/pages/Annotation/components/Toolbox/index.js
@@ -1,11 +1,48 @@
-import React from 'react'
 
-const Toolbox = (props) => {
-  return (
-    <div>
-      Toolbox
-    </div>
-  )
+import React, { Component } from 'react'
+
+import ToolboxButton from './ToolboxButton'
+import {MODE} from '../../../../constant'
+
+import {ReactComponent as RectangleIcon} from './ToolboxIcon/rectangle.svg'
+import {ReactComponent as PolygonIcon} from './ToolboxIcon/polygon.svg'
+import {ReactComponent as CursorIcon} from './ToolboxIcon/cursor.svg'
+export default class Toolbox extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentMode: MODE.CURSOR
+    }
+
+  }
+  render() {
+    return (
+      <div>
+        <ToolboxButton
+          mode={MODE.CURSOR}
+          onClickHandler={this.changeMode}
+          currentMode={this.state.currentMode}
+          component={<CursorIcon/>}
+        />
+        <ToolboxButton
+          mode={MODE.RECTANGLE}
+          onClickHandler={this.changeMode}
+          currentMode={this.state.currentMode}
+          component={<RectangleIcon/>}
+        />
+        <ToolboxButton
+          mode={MODE.POLYGON}
+          onClickHandler={this.changeMode}
+          currentMode={this.state.currentMode}
+          component={<PolygonIcon/>}
+        />
+      </div>
+    )
+  }
+  changeMode = (mode) => {
+    const {handleChangeMode} = this.props
+    this.setState({currentMode: mode})
+    if (handleChangeMode)
+      handleChangeMode(mode)
+  }
 }
-
-export default Toolbox
