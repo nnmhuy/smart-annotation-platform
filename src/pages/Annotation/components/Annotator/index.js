ce5eeb6fc6ca54edd55c175c027fd50983b57f06
Nguyễn Ngọc Minh Huy <nnmhuy@apcs.vn>
2021-03-20 11:03:21 +0700

    using konva

--- src/pages/Annotation/components/Annotator/index.js
+++ src/pages/Annotation/components/Annotator/index.js
@@ -1,36 +1,120 @@
 import React from 'react'
-import { 
-  MapContainer, ImageOverlay,
-} from 'react-leaflet'
-import LeafletGeoman from './LeafletGeoman'
+import { Stage, Layer } from 'react-konva'
+
+import Rectangle from './Rectangle'
 
 const Annotator = (props) => {
-  const { setMap } = props
+  const { rectangles, setRectangles } = props
+  const stageRef = React.createRef()
+
+  const [selectedId, selectShape] = React.useState(null)
+  const [viewportStartPos, setViewportStartPos] = React.useState(null)
+
+  const handleDragStart = (e) => {
+    e.evt.preventDefault();
+
+    const stage = stageRef.current
+    const pointer = stage.getPointerPosition();
+
+    setViewportStartPos(pointer)
+  }
+
+  const handleDragEnd = (e) => {
+    e.evt.preventDefault();
+
+    setViewportStartPos(null)
+  }
+
+  const handleDragMove = (e) => {
+    e.evt.preventDefault();
+
+    if (viewportStartPos) {
+      const stage = stageRef.current
+  
+      const pointer = stage.getPointerPosition();
+      const stagePos = stage.position()
+  
+      const newPos = {
+        x: stagePos.x + (pointer.x - viewportStartPos.x),
+        y: stagePos.y + (pointer.y - viewportStartPos.y)
+      }
+      stage.position(newPos);
+      stage.batchDraw();
+  
+      setViewportStartPos(pointer)
+    }
+  } 
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+      handleDragStart(e);
+    }
+  };
+
+  const handleZoom = (e) => {
+    e.evt.preventDefault();
+
+    const stage = stageRef.current
+    const scaleBy = 1.05;
+    const oldScale = stage.scaleX();
+
+    const pointer = stage.getPointerPosition();
+
+    const mousePointTo = {
+      x: (pointer.x - stage.x()) / oldScale,
+      y: (pointer.y - stage.y()) / oldScale,
+    };
+
+    const newScale =
+      e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    stage.scale({ x: newScale, y: newScale });
+
+    const newPos = {
+      x: pointer.x - mousePointTo.x * newScale,
+      y: pointer.y - mousePointTo.y * newScale,
+    };
+    stage.position(newPos);
+    stage.batchDraw();
+  }
+
   return (
-    <MapContainer
-      center={[0, 0]} 
-      bounds={[
-        [-50, -50],
-        [50, 50]
-      ]}
-      zoom={3}
-      style={{
-        height: '100%',
-        width: '100%',
-        overflow: 'hidden'
-      }}
+    <Stage
+      ref={stageRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={checkDeselect}
+      onMouseMove={handleDragMove}
+      onMouseUp={handleDragEnd}
+      onTouchStart={checkDeselect}
+      onWheel={handleZoom}
+      // onDragStart={handleDragStart}
+      // onDragEnd={handleDragEnd}
+      // onDragMove={handleDragMove}
     >
-      <ImageOverlay
-        url="https://image.shutterstock.com/mosaic_250/2389049/1365289022/stock-photo-a-surreal-image-of-an-african-elephant-wearing-black-and-white-zebra-stripes-1365289022.jpg"
-        bounds={[
-          [-40, -40],
-          [40, 40]
-        ]}
-        opacity={1}
-        zIndex={10}
-      />
-      <LeafletGeoman setMap={setMap}/>
-    </MapContainer>
+      <Layer>
+        {rectangles.map((rect, i) => {
+          return (
+            <Rectangle
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          );
+        })}
+      </Layer>
+    </Stage>
   );
 }
 
--- src/pages/Annotation/index.js
+++ src/pages/Annotation/index.js
@@ -8,6 +8,9 @@ import Annotator from './components/Annotator'
 
 const useStyles = makeStyles(() => ({
   root: {
+    // background: '#f8f8f8'
+  },
+  annotatorContainer: {
     background: '#f8f8f8'
   }
 }))
@@ -16,24 +19,47 @@ const GridContainer = styled(Grid)({
   height: '100vh'
 })
 
+const initialRectangles = [
+  {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: 'red',
+    opacity: 0.2,
+    stroke: 'black',
+    strokeWidth: 3,
+    id: 'rect1',
+  },
+  {
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: 'green',
+    opacity: 0.2,
+    stroke: 'black',
+    strokeWidth: 3,
+    id: 'rect2',
+  },
+];
+
 const Annotation = (props) => {
   const classes = useStyles()
-
-  const [map, setMap] = React.useState(null)
+  const [rectangles, setRectangles] = React.useState(initialRectangles);
 
   return (
     <GridContainer container className={classes.root}>
-      {/* <GridContainer container item xs={1}>
-        <Toolbox
-          map={map}
-        />
-      </GridContainer> */}
-      <GridContainer container item xs={9}>
+      <GridContainer container item xs={1}>
+        <Toolbox/>
+      </GridContainer>
+      <GridContainer container item xs={9} className={classes.annotatorContainer}>
         <Annotator
-          setMap={setMap}
+          rectangles={rectangles}
+          setRectangles={setRectangles}
         />
       </GridContainer>
-      <GridContainer container item xs={3}>
+      <GridContainer container item xs={2}>
         <Sidebar/>
       </GridContainer>
     </GridContainer>
