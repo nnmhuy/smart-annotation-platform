0b61c07d04dfb211d90a8d7c3071ce1f07c551f7
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-08-26 09:02:47 +0700

    Update FrameItem.js
    smaller font size for selected frame item

--- src/pages/Annotation/components/PropagationControl/components/VideoPropagationControl/LightFrameCarousel/FrameItem.js
+++ src/pages/Annotation/components/PropagationControl/components/VideoPropagationControl/LightFrameCarousel/FrameItem.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     'user-select': 'none'          /* Likely future */
   },
   activeFrameIndex: {
-    fontSize: 16,
+    fontSize: 15,
     fontWeight: 'bold',
     color: theme.palette.primary.main
   },
